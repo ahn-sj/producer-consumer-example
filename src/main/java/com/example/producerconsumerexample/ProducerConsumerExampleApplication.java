9c997d5e8238c12f968f3e97351a6f0603e13164
ahn-sj <jaesa5221@gmail.com>
2024-04-21 10:39:30 +0900

    Initial commit

---
+++ src/main/java/com/example/producerconsumerexample/ProducerConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.producerconsumerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerConsumerExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/producerconsumerexample/ProducerConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerconsumerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerConsumerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
